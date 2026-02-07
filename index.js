d34825178d96277f07d0b70b7077bb6eda943c50
David Lazic <lazic.ddavid@gmail.com>
2026-02-07 17:58:38 +0100

    added inputUser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import accounts from "./account/accounts.js";
 
+// ---------------- DOM ----------------
 const DOM = {
   loginBtn: document.querySelector(".login-btn"),
   navBar: document.querySelector(".nav-bar"),
@@ -8,20 +9,38 @@ const DOM = {
   inputPin: document.querySelector(".login-pin"),
 };
 
-
-accounts.forEach(account => {
+// ---------------- USERNAME (inicijali) ----------------
+accounts.forEach((account) => {
   const parts = account.owner.toLowerCase().split(" ");
   let username = "";
 
-  parts.forEach(word => {
+  parts.forEach((word) => {
     username += word[0];
   });
 
+  account.username = username;
+});
 
-
-DOM.loginBtn.addEventListener("click", (e) => {
+// ---------------- LOGIN ----------------
+DOM.loginBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  DOM.navBar.classList.add("hidden");
-  DOM.dashboard.classList.remove("hidden");
+  const enteredUser = DOM.inputUser.value;
+  const enteredPin = Number(DOM.inputPin.value);
+
+  let isLoggedIn = false;
+
+  accounts.forEach((account) => {
+    if (account.username === enteredUser && account.pin === enteredPin) {
+      isLoggedIn = true;
+
+      DOM.navBar.classList.add("hidden");
+      DOM.dashboard.classList.remove("hidden");
+
+      console.log("Ulogovan:", account.owner);
+    }
+  });
+
+  DOM.inputUser.value = "";
+  DOM.inputPin.value = "";
 });
