685622a6caf2a353104596ee657d50039537212b
David Lazic <lazic.ddavid@gmail.com>
2026-02-13 12:10:40 +0100

    Added currentAccount

--- index.js
+++ index.js
@@ -13,27 +13,55 @@ const DOM = {
 };
 let currentAccount;
 
-accounts.find((account) => {
+accounts.forEach(function (account) {
   const initials = account.owner.toLowerCase().split(" ");
   let username = "";
 
-  initials.find((word) => {
-    //find?
+  initials.forEach(function (word) {
     username += word[0];
   });
 
   account.username = username;
 });
 
-DOM.loginBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+function calculateBalance(account) {
+  let total = 0;
+
+  account.movements.forEach(function (movement) {
+    total += movement;
+  });
+
+  return total;
+}
+
+//prikaz transakcija
+function renderTransactions() {
+  DOM.transactionList.innerHTML = "";
+
+  currentAccount.movements.forEach(function (movement) {
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+      <div class="transaction ${type}">
+        <span>${type}</span>
+        <span>${movement}$</span>
+      </div>
+    `;
+
+    DOM.transactionList.insertAdjacentHTML("afterbegin", html);
+  });
+}
+//lis.
+
+DOM.loginBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
   const enteredUser = DOM.inputUser.value;
   const enteredPin = Number(DOM.inputPin.value);
 
-  const activeUser = accounts.find(
-    (account) => account.username === enteredUser && account.pin === enteredPin,
-  );
+  const activeUser = accounts.find(function (account) {
+    return account.username === enteredUser && account.pin === enteredPin;
+  });
 
   if (activeUser) {
     currentAccount = activeUser;
@@ -45,6 +73,39 @@ DOM.loginBtn.addEventListener("click", function (e) {
   } else {
     console.log("pogresan user ili pin");
   }
+
   DOM.inputUser.value = "";
   DOM.inputPin.value = "";
+});
+
+DOM.transferBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const receiverUsername = DOM.transferTo.value;
+  const amount = Number(DOM.transferAmount.value);
+
+  const receiverAccount = accounts.find(function (account) {
+    return account.username === receiverUsername;
+  });
+
+  const currentBalance = calculateBalance(currentAccount);
+
+  if (
+    receiverAccount &&
+    receiverAccount.username !== currentAccount.username &&
+    amount > 0 &&
+    amount <= 10000 &&
+    currentBalance >= amount
+  ) {
+    // skini novac sa trenutnog naloga
+    currentAccount.movements.push(-amount);
+
+    // dodaj novac primaocu
+    receiverAccount.movements.push(amount);
+
+    renderTransactions();
+  }
+
+  DOM.transferTo.value = "";
+  DOM.transferAmount.value = "";
 });
