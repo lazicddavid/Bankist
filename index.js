52cb3ddcc57a49394604ba2711db1bfb311e6b05
David Lazic <lazic.ddavid@gmail.com>
2026-02-25 21:27:59 +0100

    Added renderCurrentDAteTime

--- index.js
+++ index.js
@@ -144,6 +144,34 @@ DOM.transferBtn.addEventListener("click", function (event) {
 
 
 
+function renderCurrentDateTime() {
+  const now = new Date();
+
+  const day = String(now.getDate()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const year = now.getFullYear();
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  const formattedDateTime =
+    day +
+    "/" +
+    month +
+    "/" +
+    year +
+    ", " +
+    hours +
+    ":" +
+    minutes;
+
+  DOM.currentDate.textContent =
+    "As of " + formattedDateTime;
+
+
+
+
+
 
 
 
